fb7509644c9d2a8bf043625f6a21bb8fddb8392a
Abel Alonso <abel.alonso@gmail.com>
2018-06-28 21:20:57 +0200

    with first page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 window.onload = function(){
     var game=new Game("triste");
     var soundID = "Thunder";
+    var audio = new Audio("resources/tetristheme.mp3");
 
-    $("#start").click(function (e){
+    $(".btn").click(function (e){
         $("#start").text("Pause");
         $("#stop").text("Resume");
         $("#start").toggleClass("btn-danger");
@@ -10,30 +11,52 @@ window.onload = function(){
         $("#start").attr("id", "stop");
         $("#stop").attr("id", "start");
         game.start();
+        audio.play();
     });
     
     $("#classic").click(function (e){
+        audio.pause();
         $(".level").prop("disable", false);
         $("#classic").prop("disable", true);
         console.log("classic");
-        var audio = new Audio("resources/tetristheme.mp3");
+        audio = new Audio("resources/tetristheme.mp3");
         audio.play();
+        $("body").css({
+            "background-image": "url(resources/tetris.jpg)",
+            "background-repeat": "no-repeat",
+            "background-size": "cover"});
+            
+        $("label").css({color: "white"});
         //
     })
     $("#relaxed").click(function (e){
+        audio.pause();
         $(".level").prop("disable", false);
         $("#relaxed").prop("disable", true);
         console.log("relaxed");
-        var audio = new Audio("resources/tetrisreggae.mp3");
+        audio = new Audio("resources/tetrisreggae.mp3");
         audio.play();
+        $("body").css({
+            "background-image": "url(resources/bigphotoformaspalomas.jpg)",
+            "background-repeat": "no-repeat",
+            "background-size": "cover"});
+            
+        $("label").css({color: "white"});
         //
-    })
+    });
     $("#exciting").click(function (e){
+        audio.pause();
         $(".level").prop("disable", false);
         $("#exciting").prop("disable", true);
         console.log("exciting");
-        var audio = new Audio("resources/tetrismetal.mp3");
+        audio = new Audio("resources/tetrismetal.mp3");
         audio.play();
+        $("body").css({
+            "background-image": "url(resources/heavymetal.jpg)",
+            "background-repeat": "no-repeat",
+            "background-size": "cover"});
+            
+        $("label").css({color: "white"});
     })
 
 };
